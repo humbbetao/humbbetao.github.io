a1c6da2d1247f0264476beebe4ce0c7bdc4551eb
Humberto Gonçalves <humberto.g.moreira@gmail.com>
2020-03-07 21:29:21 -0300

    Updates

---
+++ precache-manifest.87899c5e12f897e31bd852b32403de39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78cfd401610a08f3488473d2bf6c38f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49457def142f44b7512f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bbe5dd23591757a37523",
+    "url": "/static/js/2.d3e62244.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d3e62244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49457def142f44b7512f",
+    "url": "/static/js/main.583191b2.chunk.js"
+  },
+  {
+    "revision": "4b866dd6eb45cb996168",
+    "url": "/static/js/runtime-main.667c864f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
